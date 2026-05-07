5095ffc30716f58e2af231c12a01cc15bd6d532d
AmigoLionXP <contact@lionxp.ch>
2026-05-07 23:27:09 +0200

    refactor: Add MOOVICITY brand colors to theme palette

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,12 +1,20 @@
 export const colors = {
+  // Primary MOOVICITY colors
+  darkTeal: '#004D4D',
+  lightTeal: '#1a7a7a',
+  
+  // Status colors
   primary: '#007AFF',
   secondary: '#5AC8FA',
-  success: '#4CD964',
+  success: '#2ECC71',
   danger: '#FF3B30',
-  warning: '#FF9500',
+  warning: '#E67E22',
+  
+  // Neutral colors
   background: '#F5F5F5',
   white: '#FFFFFF',
   black: '#000000',
   gray: '#8E8E93',
   lightGray: '#D1D1D6',
+  darkText: '#333333',
 };
